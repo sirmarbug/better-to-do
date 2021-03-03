f791cc88edba1494b9981feb2d65d0a25f463a55
Mariusz Bugajski <mbugajski.dev@gmail.com>
2021-03-03 21:13:46 +0100

    add global styles

--- src/main.ts
+++ src/main.ts
@@ -10,5 +10,6 @@ import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
+import './styles/styles.scss'
 
 createApp(App).use(PrimeVue).use(i18n).use(store).use(router).mount('#app')
