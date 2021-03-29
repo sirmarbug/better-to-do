ae99381c2e2e8caf033c8f99cd04b3c35e97017d
Mariusz Bugajski <mbugajski.dev@gmail.com>
2021-03-29 21:57:55 +0200

    refactor firebase

--- src/store/modules/auth/actions.ts
+++ src/store/modules/auth/actions.ts
@@ -1,27 +1,11 @@
 import { ActionTree } from 'vuex'
 import { AppState, AuthState, UserForm } from '@/types'
-import { auth } from '@/firebase'
-
-const fakeApi = () => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve({ username: 'adamkowski' })
-    }, 1000)
-  })
-}
+import { createUser, loginWithEmail } from '@/firebase/api'
 
 export const actions: ActionTree<AuthState, AppState> = {
-  async fetchUser ({ commit }) {
-    try {
-      const response = await fakeApi()
-      commit('SET_USER', response)
-    } catch (e) {
-      console.error(e)
-    }
-  },
   async createUser ({ commit }, user: UserForm) {
     try {
-      const response = await auth.createUserWithEmailAndPassword(user.email, user.password)
+      const response = await createUser(user)
       console.log(response)
     } catch (e) {
       console.error(e)
@@ -29,7 +13,7 @@ export const actions: ActionTree<AuthState, AppState> = {
   },
   async loginWithEmail ({ state }, payload: UserForm) {
     try {
-      const response = await auth.signInWithEmailAndPassword(payload.email, payload.password)
+      const response = await loginWithEmail(payload)
       console.log(response)
     } catch (e) {
       console.error(e)
